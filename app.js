e6b4b05cfd7494f5d94214541eb9131bcf47f0ab
Mohd Shahid Khan <khan42290@gmail.com>
2021-11-11 15:42:54 +0530

    added parameter for url

--- app.js
+++ app.js
@@ -6,3 +6,11 @@ var translatedText = document.getElementById('translated-text');
 var minionURL = "https://api.funtranslations.com/translate/minion.json";
 
 
+// construct URL
+
+function addTextParameterToURL(textParameter) {
+    var endcodedURI = encodeURIComponent(textParameter);
+    return `${minionURL}?text=${endcodedURI}`;
+  }
+  
+ 
