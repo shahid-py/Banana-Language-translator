cca81c2e5d4a48c7f89b713a9c7dbeb1a84266a9
Mohd Shahid Khan <khan42290@gmail.com>
2021-11-11 15:36:30 +0530

    input(id and API url)

---
+++ app.js
@@ -0,0 +1,8 @@
+var btnTranslate = document.getElementById('btn-translate');
+
+var inputText = document.getElementById('input-text');
+var translatedText = document.getElementById('translated-text');
+
+var minionURL = "https://api.funtranslations.com/translate/minion.json";
+
+
