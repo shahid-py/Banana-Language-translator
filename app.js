2ef5a4c042c096d4016d7181cc5db51357bd8c10
Mohd Shahid Khan <khan42290@gmail.com>
2021-11-11 15:46:58 +0530

    if else

--- app.js
+++ app.js
@@ -14,9 +14,30 @@ function addTextParameterToURL(textParameter) {
   }
   
  // click handler function
- 
+
 function clickHandler() {
     var textParameter = inputText.value;
     var serverURL = addTextParameterToURL(textParameter);
   
     console.log(serverURL);
+
+
+    if (textParameter.length === 0) {
+        alert("You need to write a sentence");
+
+      } else if (/^\d+$/.test(textParameter)) {
+        alert("You need to include letters in your sentence");
+        
+      } else {
+        fetch(serverURL)
+        .then(response => response.json())
+        .then(json => { 
+          translatedText.innerText = json.contents.translated;
+        })
+        .catch(error => {
+          console.log('error occured', error);
+        })
+      }
+    }
+    
+    btnTranslate.addEventListener('click', clickHandler);
